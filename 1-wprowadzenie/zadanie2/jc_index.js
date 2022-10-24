01e676eb284bb8323d869c5f80157bc72116cfb8
Julka <j.a.czapiewska@gmail.com>
2022-10-24 20:43:07 +0200

    wprowadzenie - 2

---
+++ 1-wprowadzenie/zadanie2/jc_index.js
@@ -0,0 +1,17 @@
+var one = true; //boolean
+var two = 23.456; //number
+var three = "tekst"; //string
+var four = null; //null
+var five; //undefined
+var six = NaN; //not a number
+var seven = ["jeden", "dwa", "trzy"]; //array = tablica
+var eight = {name: "Julia", age: 28}; //object
+
+console.log(typeof one);
+console.log(typeof two);
+console.log(typeof three);
+console.log(typeof four);
+console.log(typeof five);
+console.log(typeof six);
+console.log(typeof seven);
+console.log(typeof eight);
