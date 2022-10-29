fab2c79861fc2fa191ff1b910e93f24ef229fd14
Julka <j.a.czapiewska@gmail.com>
2022-10-29 21:46:06 +0200

    funkcje - zadanie 3

---
+++ 2-funkcje/zadanie3/jc-index.js
@@ -0,0 +1,11 @@
+// 3.2
+
+function kata () {
+    function ninja () {
+        console.log("Jestem ninja!");
+    }
+    ninja();
+}
+
+kata();
+// ninja(); nie można wywołać poza scopem wewnętrznej funkcji
